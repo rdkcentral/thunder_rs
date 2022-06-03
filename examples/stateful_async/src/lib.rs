4e0c2ad3d87c1cdcdfdc2d8e5865c4a9968e662b
Kevin Pearson <kevin_pearson@cable.comcast.com>
2022-06-03 13:36:58 -0700

    fmt

--- examples/stateful_async/src/lib.rs
+++ examples/stateful_async/src/lib.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tokio::{runtime::Runtime, sync::mpsc};
 
@@ -21,109 +21,117 @@ use tokio::{runtime::Runtime, sync::mpsc};
  * limitations under the License.
  */
 struct StatefulPlugin {
-  plugin_tx: mpsc::Sender<PluginRequest>,
-  _rt: Runtime,
+    plugin_tx: mpsc::Sender<PluginRequest>,
+    _rt: Runtime,
 }
 
 struct PluginRequest {
-  json: String,
-  ctx: thunder_rs::RequestContext,
+    json: String,
+    ctx: thunder_rs::RequestContext,
 }
 
 struct StatefulService {
-  state: String,
+    state: String,
 }
 
 #[derive(Serialize)]
 struct JsonRpcResponse {
-  jsonrpc: String,
-  id: u32,
-  result: Value,
+    jsonrpc: String,
+    id: u32,
+    result: Value,
 }
 
 #[derive(Serialize, Deserialize)]
 struct JsonRpcRequest {
-  jsonrpc: String,
-  id: u32,
-  method: String,
-  params: Option<Value>,
+    jsonrpc: String,
+    id: u32,
+    method: String,
+    params: Option<Value>,
 }
 
 impl StatefulService {
-  async fn state_async(&self) -> String {
-    String::from(self.state.clone())
-  }
-  fn set_state(&mut self, state: String) {
-    self.state = state
-  }
+    async fn state_async(&self) -> String {
+        String::from(self.state.clone())
+    }
+    fn set_state(&mut self, state: String) {
+        self.state = state
+    }
 
-  async fn handle_rpc(&mut self, jsonrpc: String) -> String {
-    let req: JsonRpcRequest = serde_json::from_str(&jsonrpc).unwrap();
-    println!("Handle RPC call {}", req.method.as_str());
-    let val = match req.method.as_str() {
-      "State.set" => {
-        let ps = req.params.unwrap();
-        let new_state = ps.as_str().unwrap();
-        self.set_state(String::from(new_state));
-        Value::Null
-      },
-      "State.get" => {
-        Value::String(self.state_async().await)
-      },
-      _ => {
-        //TODO Should really return an error
-        Value::Null
-      }
-    };
-    let resp = JsonRpcResponse {
-      jsonrpc: String::from("2.0"),
-      id: req.id,
-      result: val,
-    };
-    serde_json::to_string(&resp).unwrap()
-  }
+    async fn handle_rpc(&mut self, jsonrpc: String) -> String {
+        let req: JsonRpcRequest = serde_json::from_str(&jsonrpc).unwrap();
+        println!("Handle RPC call {}", req.method.as_str());
+        let val = match req.method.as_str() {
+            "State.set" => {
+                let ps = req.params.unwrap();
+                let new_state = ps.as_str().unwrap();
+                self.set_state(String::from(new_state));
+                Value::Null
+            }
+            "State.get" => Value::String(self.state_async().await),
+            _ => {
+                //TODO Should really return an error
+                Value::Null
+            }
+        };
+        let resp = JsonRpcResponse {
+            jsonrpc: String::from("2.0"),
+            id: req.id,
+            result: val,
+        };
+        serde_json::to_string(&resp).unwrap()
+    }
 }
 
 impl thunder_rs::Plugin for StatefulPlugin {
-  fn on_message(&mut self, json: String, ctx: thunder_rs::RequestContext) {
-    println!("\tchannel:{0}", ctx.channel);
-    println!("\tauth_token:{0}", ctx.auth_token);
-    let tx = self.plugin_tx.clone();
-    async_std::task::block_on(async {
-      let r = tx.send(PluginRequest { json: json, ctx: ctx }).await;
-      match r {
-          Ok(_) => println!("Successfully sent!"),
-          Err(err) => println!("ERR {}", err),
-      }
-    });
-  }
+    fn on_message(&mut self, json: String, ctx: thunder_rs::RequestContext) {
+        println!("\tchannel:{0}", ctx.channel);
+        println!("\tauth_token:{0}", ctx.auth_token);
+        let tx = self.plugin_tx.clone();
+        async_std::task::block_on(async {
+            let r = tx
+                .send(PluginRequest {
+                    json: json,
+                    ctx: ctx,
+                })
+                .await;
+            match r {
+                Ok(_) => println!("Successfully sent!"),
+                Err(err) => println!("ERR {}", err),
+            }
+        });
+    }
 
-  fn on_client_connect(&mut self, channel: u32) {
-    println!("client_connect:{}", channel);
-  }
+    fn on_client_connect(&mut self, channel: u32) {
+        println!("client_connect:{}", channel);
+    }
 
-  fn on_client_disconnect(&mut self, channel: u32) {
-    println!("client_disconnect:{}", channel);
-  }
+    fn on_client_disconnect(&mut self, channel: u32) {
+        println!("client_disconnect:{}", channel);
+    }
 }
 
 fn stateful_plugin_init() -> Box<dyn thunder_rs::Plugin> {
-  let _rt = Runtime::new().unwrap();
-  let _guard = _rt.enter();
+    let _rt = Runtime::new().unwrap();
+    let _guard = _rt.enter();
 
-  let (plugin_tx, mut plugin_rx) = mpsc::channel::<PluginRequest>(32);
-  tokio::spawn(async move {
-    let mut service = StatefulService {
-      state: String::from("Initial"),
-    };
-    while let Some(msg) = plugin_rx.recv().await {
-      let s = service.handle_rpc(msg.json).await;
-      println!("Sending {}", s);
-      msg.ctx.responder.send(thunder_rs::Message{ channel: msg.ctx.channel, data: s })
-      .unwrap();
-    }
-  });
-  Box::new(StatefulPlugin{ plugin_tx, _rt })
+    let (plugin_tx, mut plugin_rx) = mpsc::channel::<PluginRequest>(32);
+    tokio::spawn(async move {
+        let mut service = StatefulService {
+            state: String::from("Initial"),
+        };
+        while let Some(msg) = plugin_rx.recv().await {
+            let s = service.handle_rpc(msg.json).await;
+            println!("Sending {}", s);
+            msg.ctx
+                .responder
+                .send(thunder_rs::Message {
+                    channel: msg.ctx.channel,
+                    data: s,
+                })
+                .unwrap();
+        }
+    });
+    Box::new(StatefulPlugin { plugin_tx, _rt })
 }
 
-thunder_rs::export_plugin!("StatefulRustPlugin", (1,0,0), stateful_plugin_init);
+thunder_rs::export_plugin!("StatefulRustPlugin", (1, 0, 0), stateful_plugin_init);
